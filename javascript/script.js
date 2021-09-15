3b1b74dba275cbf14ca39db55222b11d5c8474e2
Jack-rm <jackcnsr94@gmail.com>
2021-09-16 01:07:29 +0200

    Testing vue transitions' features

--- javascript/script.js
+++ javascript/script.js
@@ -13,34 +13,35 @@ let root = new Vue(
             images: [
                 {
                     source: 'https://static0.srcdn.com/wordpress/wp-content/uploads/2020/05/Lord-of-the-Rings-Gandalf-Grey-and-White.jpg',
-                    alt: 'wizard image of Gandalf',
+                    alt: 'Gandalf',
                 },
                 {
                     source: 'https://imperodisney.files.wordpress.com/2018/01/merlino-merlin-la-spada-nella-roccia-disney-live-action.jpg',
-                    alt: 'wizard image of Merlin',
+                    alt: 'Merlin',
                 },
                 {
                     source: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/07/24/17/Albus-Dumbledore.jpg?width=1200',
-                    alt: 'wizard image of Dumbledore',
+                    alt: 'Dumbledore',
                 },
                 {
                     source: 'https://starwarsblog.starwars.com/wp-content/uploads/sites/6/2017/05/yoda-advice-always-with-you-what-cannot-be-done.jpg',
-                    alt: 'wizard image of Yoda',
+                    alt: 'Yoda',
                 },
                 {
                     source: 'https://tech.everyeye.it/public/immagini/21122020/curiosita-storiche_notizia-2.jpg',
-                    alt: 'wizard image of Houdini',
+                    alt: 'Houdini',
                 },
                 {
                     source: 'https://upload.wikimedia.org/wikipedia/it/thumb/d/db/Aladdin_-_Jafar.JPG/1200px-Aladdin_-_Jafar.JPG',
-                    alt: 'wizard image of Jafar',
+                    alt: 'Jafar',
                 },
                 {
                     source: 'https://www.ciakclub.it/wp-content/uploads/2020/05/MV5BYTM4YWY4YWMtOWE2Ny00MjRhLWE3YWQtNzIyNTFhYzM3MmE2XkEyXkFqcGdeQXVyOTc5MDI5NjE@._V1_SX1777_CR001777755_AL_.jpg',
-                    alt: 'wizard image of David Bowie',
+                    alt: 'David Bowie',
                 },
             ],
             imageIndex: 0,
+            show: false,
         },
         methods: {
             beforeImg: function(){  
