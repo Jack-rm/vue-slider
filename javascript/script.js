b832e0b59b8390516425aadf58f97c64dc6b85a0
Jack-rm <jackcnsr94@gmail.com>
2021-09-15 16:05:05 +0200

    Initialization of Vuejs slider exercise

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+/**
+ * Descrizione:
+ * Rifare l'esercizio dello slider come fatto assieme in classe, e una volta raggiunto il risultato richiesto,
+ * sperimentate e provate ad implementare quello che volete, anche qualche funzionalità particolare!
+ * 
+ */
